5e7ae4c917bd2ae4b4d600fde58b214ba8ad9827
Dhruv Jha <omdhruvjha@gmail.com>
2021-01-18 18:55:24 +0530

    v2

---
+++ scripts/drawer.js
@@ -0,0 +1,20 @@
+document.getElementById("insta").addEventListener("click", insta);
+document.getElementById("fbook").addEventListener("click", fbook);
+document.getElementById("pint").addEventListener("click", pint);
+document.getElementById("linkd").addEventListener("click", linkd);
+
+function insta() {
+  location = "https://instagram.com";
+}
+
+function fbook() {
+  location = "https://facebook.com";
+}
+
+function pint() {
+  location = "https://pinterest.com";
+}
+
+function linkd() {
+  location = "https://linkedin.com";
+}
